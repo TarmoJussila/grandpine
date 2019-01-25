1c7fef165c2c0c9035c75a16009bf7a0af40aba6
Tarmo Jussila <tarmo.jussila@gmail.com>
2019-01-25 23:20:49 +0200

    Testing camera stuff.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject player;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
