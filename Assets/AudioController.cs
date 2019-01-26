f4cbe808955e173bf76d70b5f4c025e85431b8ce
Tarmo Jussila <tarmo.jussila@gmail.com>
2019-01-26 20:39:43 +0200

    Audio controller. Not in scene yet.

---
+++ Assets/AudioController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public enum SoundType
+{
+    Walk1,
+    Walk2,
+    AxeHit,
+    TreeHit1,
+    TreeHit2,
+    TreFall,
+}
+
+[System.Serializable]
+public class Sound
+{
+    public SoundType SoundType;
+    public AudioClip AudioClip;
+}
+
+public class AudioController : Singleton<AudioController>
+{
+    [Header("References")]
+    [SerializeField] private AudioSource ambientAudioSource;
+    [SerializeField] private AudioSource soundAudioSource;
+
+    [Header("Settings")]
+    [SerializeField] private List<Sound> sounds = new List<Sound>();
+
+    private Dictionary<SoundType, AudioClip> soundsDictionary = new Dictionary<SoundType, AudioClip>();
+
+    private void Start()
+    {
+        soundsDictionary = sounds.ToDictionary(x => x.SoundType, x => x.AudioClip);
+    }
+
+    public void PlaySound(SoundType soundType)
+    {
+        soundAudioSource.PlayOneShot(soundsDictionary[soundType]);
+    }
+}
