f236ed70dbaeb601e25ee6be2902fcd054961612
Tarmo Jussila <tarmo.jussila@gmail.com>
2019-01-27 13:43:58 +0200

    Glass dome material WIP. Game state for activating camera zoom out. WIP.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public enum GameState
 {
     Game,
-    Credits
+    GameOver,
+    Credits,
 }
 
 public class GameController : Singleton<GameController>
 {
+    private GameState currentGameState = GameState.Game;
+
     private readonly int targetFps = 60;
 
     private void Start()
     {
         Application.targetFrameRate = targetFps;
+    }
+
+    public void SetGameState(GameState gameState)
+    {
+        if (currentGameState != gameState)
+        {
+            currentGameState = gameState;
+
+            if (gameState == GameState.GameOver)
+            {
+                CameraController.Instance.ZoomOut(0.5f, 10f);
+            }
+        }
     }
 }
