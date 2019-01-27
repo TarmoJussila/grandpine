bacaaab6869e129794131e14534ab9fb519a7f0b
Tarmo Jussila <tarmo.jussila@gmail.com>
2019-01-27 13:09:26 +0200

    Added fade controller and initial fade out.

---
+++ Assets/FadeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeController : Singleton<FadeController>
+{
+    private Image fadeImage;
+
+    private float fadeValue = 1f;
+    private float targetFadeValue = 0f;
+    private float fadeSpeed = 0.5f;
+
+    private void Start()
+    {
+        fadeImage = GetComponentInChildren<Image>();
+
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOut()
+    {
+        fadeValue = 1f;
+        targetFadeValue = 0f;
+
+        while (fadeValue > targetFadeValue)
+        {
+            fadeValue = Mathf.Max(fadeValue - Time.deltaTime * fadeSpeed, targetFadeValue);
+
+            fadeImage.color = new Color(0, 0, 0, fadeValue);
+
+            yield return null;
+        }
+
+        fadeImage.color = Color.clear;
+    }
+}
